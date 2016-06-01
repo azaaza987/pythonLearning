e723f98cff4dc56beae8723d5c4c30c0b0239061
liangliangyy@gmail.com <liangliangyy@gmail.com>
2016-06-01 23:21:22 +0800

    s

--- cnBlogRss.py
+++ cnBlogRss.py
@@ -33,6 +33,10 @@ class cnBlogRss():
                                     description=str(datetime.date.today()),
                                     items=[]
                                     )
+        httpHandler = urllib2.HTTPHandler(debuglevel=1)
+        httpsHandler = urllib2.HTTPSHandler(debuglevel=1)
+        opener = urllib2.build_opener(httpHandler, httpsHandler)
+        urllib2.install_opener(opener)
         self.baseurl = "http://feed.cnblogs.com/blog/sitehome/rss"
 
     def useragent(self, url):
