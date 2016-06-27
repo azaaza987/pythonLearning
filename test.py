c24f65944ee6a90f99e3095cb13941a60c31d62a
liangliangyy <liangliangyy@gmail.com>
2016-06-27 23:22:57 +0800

    s

--- test.py
+++ test.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__ = 'liangliang'
+# encoding: utf-8
 
 
-"""blog : http://www.lylinux.org"""
+"""
+@version: ??
+@author: liangliangyy
+@license: Apache Licence 
+@contact: liangliangyy@gmail.com
+@site: https://www.lylinux.org/
+@software: PyCharm
+@file: test.py
+@time: 2016/6/2 22:40
+"""
 
-def test():
+from gevent.queue import Queue
+import gevent
+message_queue = Queue()
 
-    d={"server":"127.0.0.1","port":"80"}
+def receiver(n):
+    while not message_queue.empty():
+        message = message_queue.get()
+        print('Received %s  message %s' % (n, message))
+        gevent.sleep(2)
+
+    print('Quitting time!')
+
+def sender():
+    for i in xrange(1,25):
+        message_queue.put_nowait(i)
+
+gevent.spawn(sender).join()
+
+gevent.joinall([
+    gevent.spawn(receiver, 'steve'),
+    gevent.spawn(receiver, 'john'),
+    gevent.spawn(receiver, 'nancy'),
+    ])
 
