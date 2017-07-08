d8931da3a72f61f2963b21f77512868aed568db9
liangliang <liangliangyy@gmail.com>
2017-07-08 16:39:22 +0800

    修改歌词下载功能

--- LyricDownLoad.py
+++ LyricDownLoad.py
@@ -17,11 +17,8 @@ import requests
 import eyed3
 import re
 import json
-import sys
 import os
-
-reload(sys)
-sys.setdefaultencoding('utf8')
+from abc import ABCMeta, abstractmethod, abstractproperty
 
 musicpath = r'/Users/liangliang/Music/'
 
@@ -29,147 +26,160 @@ musicpath = r'/Users/liangliang/Music/'
 # musicpath = r'/Users/liangliang/Music/网易云音乐'
 
 
-class Base():
-    def __init__(self, name, artist):
-        self.name = name
-        self.artist = artist
-        self.havelrc = False
-
-    def useragent(self, url):
-        i_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) \
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36", \
-                     "Referer": 'http://baidu.com/'}
-        rsp = requests.get(url, headers=i_headers)
-        # print rsp.encoding
-        rsp.encoding = rsp.apparent_encoding
-        return rsp.text
-
-
 class LyricTools():
     def __init__(self, lyric):
         self.lyric = lyric
-        self.dictTimeLrc = {}
 
-    def __getstr(self, i):
+    def __getstr(self, span):
 
-        if i < 10:
-            return "0" + str(i)
-        else:
-            return str(i)
+        s = list(map(lambda i: str(i) if i >= 10 else '0' + str(i), span))
+        data = '{i}:{j}.{k}'.format(i=s[0], j=s[1], k=s[2])
+        return data
 
     def replacetimespan(self, line, refind):
         for f in refind:
             line = line.replace(f, '')
         return line
 
-    def __parselrc(self):
+    def parse_lrc_to_dict(self):
         if self.lyric == '' or self.lyric is None:
             return
-
-        pattern = re.compile(r'\[\d{2,3}:\d{2,3}\.\d{2,3}\]')
+        timelrc = dict()
+        pattern = re.compile(r'\[\d{1,3}:\d{1,3}\.\d{1,3}\]')
         for line in self.lyric.split('\n'):
             refind = pattern.findall(line)
             lrcres = self.replacetimespan(line, refind)
             for timespan in refind:
                 timespan = timespan.replace('[', '').replace(']', '')
-                # print timespan
-                self.dictTimeLrc[timespan] = lrcres
+                timelrc[timespan] = lrcres
+        return timelrc
 
-    def parseLyric(self):
-        self.__parselrc()
+    def parse_lyric_dict(self, dictTimeLrc):
+
         mylrcstr = ''
-        for i in range(00, 05):
+        for i in range(0, 5):
             for j in range(00, 59):
                 for k in range(00, 99):
-                    timespan = self.__getstr(i) + ":" + self.__getstr(j) + "." + self.__getstr(k)
-                    if self.dictTimeLrc.has_key(timespan):
-                        lrc = self.dictTimeLrc[timespan]
-                        # print lrc
+                    timespan = self.__getstr((i, j, k))
+                    if timespan in dictTimeLrc:
+                        lrc = dictTimeLrc[timespan]
+
                         mylrcstr += lrc + '\n'
-        mylrcstr = mylrcstr.decode('utf8')
-        mylrcstr = u''.join(mylrcstr)
-        mylrcstr = unicode(mylrcstr)
+
         return mylrcstr
 
 
+class Base():
+    SearchType = None
+
+    def __init__(self, name, artist):
+        self.name = name
+        self.artist = artist
+        self.lrcdict = dict()
+        self.lrcurl = ''
+        self.lrc = ''
+
+    @property
+    def have_lrc_url(self):
+        return self.lrcurl and self.lrcurl != ''
+
+    @property
+    def have_lrc_data(self):
+        return self.have_lrc_url and self.lrc
+
+    def useragent(self, url):
+        i_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36", \
+                     "Referer": 'http://baidu.com/'}
+        rsp = requests.get(url, headers=i_headers)
+        rsp.encoding = rsp.apparent_encoding
+        return rsp.text
+
+    @abstractmethod
+    def search_music(self):
+        print("{type} start search {music}-{artist}".format(music=self.name, artist=self.artist, type=self.SearchType))
+
+    @abstractmethod
+    def getlrc_content(self):
+        pass
+
+    def parselrc(self, lrc_content):
+        if lrc_content:
+            print("{type} start parse lrc  {music}-{artist}".format(music=self.name, artist=self.artist,
+                                                                    type=self.SearchType))
+            parser = LyricTools(lyric=lrc_content)
+            lrcdict = parser.parse_lrc_to_dict()
+            self.lrcdict = lrcdict
+            self.lrc = parser.parse_lyric_dict(lrcdict)
+            return self.lrc
+
+
 class BaiDuMusic(Base):
-    def __init__(self, name, artiist):
-        Base.__init__(self, name, artiist)
+    SearchType = 'baidu'
 
-    def CheckLyric(self, url):
-        html = self.useragent(url)
-        soup = BeautifulSoup(html)
-        titlespan = soup.find("span", {"class": "name"})
-        authspan = soup.find("span", {"class": "author_list"})
-        import chardet
-        print  chardet.detect(str(titlespan.text))
-        print str(titlespan.text) == self.name
+    def __init__(self, name, artist):
+        super(BaiDuMusic, self).__init__(name, artist)
 
-        print str(authspan['title']).decode('gb18030').encode('utf-8')
-
-    def Search(self):
+    def search_music(self):
+        super(BaiDuMusic, self).search_music()
         requrl = 'http://music.baidu.com/search?key=' + self.name + '+' + self.artist
-
         rsp = self.useragent(requrl)
-        with open('text.txt', 'w') as file:
-            file.writelines(rsp)
-        soup = BeautifulSoup(rsp)
+        soup = BeautifulSoup(rsp, 'lxml')
         div = soup.find("div", {"monkey": "result-song"})
-        if div is not None:
-
+        if div:
             ul = div.find('ul')
-
             for li in ul.findAll('li'):
                 a = li.find('a')
                 url = 'http://music.baidu.com/' + a.get('href')
-
                 titleem = li.find('em')
                 authorli = li.find('span', {"class": "author_list"})
                 if authorli and titleem:
                     authorem = authorli.find('em')
                     if authorem:
                         author = authorem.text
-                        # print author
+
                         title = titleem.text
-                        # print title
+
                         if author and title:
                             if self.artist == author and self.name == title:
-                                self.havelrc = True
+                                self.lrcurl = url
                                 return url
         return None
 
-    def GetLyric(self, lyricUrl):
-        if self.havelrc and lyricUrl != '':
-            print lyricUrl
-            rsp = self.useragent(lyricUrl)
+    def getlrc_content(self):
+        if self.have_lrc_url:
+            super(BaiDuMusic, self).getlrc_content()
+            rsp = self.useragent(self.lrcurl)
             soup = BeautifulSoup(rsp, "lxml")
             div = soup.find("div", {"id": "lyricCont"})
             if div == None:
                 return ''
-            # print(div)
-            # print lyricUrl
             lrcurl = div['data-lrclink']
             if lrcurl == None:
                 return ''
-            print(lrcurl)
             lrc = self.useragent(lrcurl)
+            self.lrc = lrc
             return lrc
         return None
 
 
 class XiaMiLrc(Base):
-    def __init__(self, name, artiist):
-        Base.__init__(self, name, artiist)
+    SearchType = 'xiami'
 
-    def GetLyric(self, lyricUrl):
-        if self.havelrc and lyricUrl != '' and lyricUrl != None:
-            print lyricUrl
-            data = self.useragent(lyricUrl)
+    def __init__(self, name, artist):
+        super(XiaMiLrc, self).__init__(name, artist)
+
+    def getlrc_content(self):
+        if self.have_lrc_url:
+            super(XiaMiLrc, self).getlrc_content()
+            data = self.useragent(self.lrcurl)
+            self.lrc = data
             return data
 
-    def Search(self):
+    def search_music(self):
+        super(XiaMiLrc, self).search_music()
         requrl = 'http://www.xiami.com/search/song-lyric?key=' + self.artist + '+' + self.name
-        print requrl
+
         rsp = self.useragent(requrl)
 
         soup = BeautifulSoup(rsp, 'lxml')
@@ -177,145 +187,85 @@ class XiaMiLrc(Base):
 
         table = div.findAll("table", {"class": "track_list"})
         if table is not None:
-            self.havelrc = True
             for t in table:
                 s = t.find("td", {"class": "song_name"})
                 a = t.find("td", {"class": "song_artist"})
 
                 if s and a:
-                    artist = re.sub('<(.*?)>', '', str(a)).decode('utf-8').replace('\t', '') \
+                    artist = re.sub('<(.*?)>', '', str(a)).replace('\t', '') \
                         .replace('\n', '').replace('MV%20', '').replace(' ', '')
-                    name = re.sub('<(.*?)>', '', str(s)).decode('utf-8').replace('\t', '') \
+                    name = re.sub('<(.*?)>', '', str(s)).replace('\t', '') \
                         .replace('\n', '').replace('MV%20', '').replace("MV", '').replace(' ', '')
-                    # print artist,name
-                    # print self.artist,self.name
                     try:
                         if self.artist == artist and self.name == name:
                             inputvalue = t.find(['input'])['value']
                             self.havelrc = True
                             requrl = 'http://www.xiami.com/song/playlist/id/' + inputvalue
-                            # print requrl
                             rsp = self.useragent(requrl)
 
                             lrcid = re.compile('<lyric_url>(.*?)</lyric_url>', re.S).findall(rsp)
                             lrcurl = lrcid[0]
-                            print lrcurl
+                            self.lrcurl = lrcurl
                             return lrcurl
-                            # print lrcurl
-                            # return self.__getlrcstr(lrcurl)
-                    except Exception, e:
-                        print 'xiami error'
-                        print e
-                        self.havelrc = False
+                    except Exception as e:
+                        print('xiami error')
+                        print(e)
                         return ''
 
 
 class TTpodLyric(Base):
-    def __init__(self, name, artiist):
-        Base.__init__(self, name, artiist)
+    SearchType = 'ttpod'
 
-    def GetLyric(self):
+    def __init__(self, name, artist):
+        name = str(name).replace(' ', '')
+        artist = str(artist).replace(' ', '')
+        super(TTpodLyric, self).__init__(name, artist)
+
+    def search_music(self):
+        super(TTpodLyric, self).search_music()
         url = 'http://lp.music.ttpod.com/lrc/down?artist=' + self.artist + '&title=' + self.name
-        print url
-        rsp = self.useragent(url)
-        jsondata = json.loads(rsp)
-        if str(jsondata['code']) == '1':
-            self.havelrc = True
-            return str(jsondata['data']['lrc'])
+        self.lrcurl = url
+        return url
+
+    def getlrc_content(self):
+        if self.have_lrc_url:
+            super(TTpodLyric, self).getlrc_content()
+            rsp = self.useragent(self.lrcurl)
+            jsondata = json.loads(rsp)
+            if str(jsondata['code']) == '1':
+                self.lrc = str(jsondata['data']['lrc'])
+                return self.lrc
 
 
 class MusicTools():
-    def GetLyric(self, name, artiist):
-        count = 0
-        lyric = ''
-        name = str(name).replace(' ', '')
-        artiist = str(artiist).replace(' ', '')
-        while count <= 3:
-            # baidu
-            # print name, artist
-            count += 1
-            if count == 1:
-                print 'baidu'
-                baidu = BaiDuMusic(name, artiist)
-                url = baidu.Search()
-                if not baidu.havelrc:
-                    continue
-                else:
-                    lyric = baidu.GetLyric(url)
-                    if lyric == '' or lyric == None:
-                        continue
-                    else:
-                        break
-            # xiami
-            if count == 2:
-                print 'xiami'
-                xiami = XiaMiLrc(name, artiist)
-                url = xiami.Search()
-                if xiami.havelrc:
-                    lyric = xiami.GetLyric(url)
+    def __get_music_apps(self, name, artist):
+        applications = Base.__subclasses__()
+        return list(map(lambda x: x(name, artist), applications))
 
-                    if lyric == '' or lyric == None:
-                        continue
-                    else:
-                        break
-                else:
-                    continue
-            # ttpod
-            if count == 3:
-                print 'ttpod'
-                ttpod = TTpodLyric(name, artiist)
-                lyric = ttpod.GetLyric()
-                if ttpod.havelrc:
-                    break
-                else:
-                    continue
-        if lyric != '':
-            parser = LyricTools(lyric=lyric)
-            data = parser.parseLyric()
-            return data
+    def GetLyric(self, name, artist):
+        apps = self.__get_music_apps(name, artist)
+        for app in apps:
+            app.search_music()
+            app.getlrc_content()
+            if app.have_lrc_data:
+                print('{apptype} get lrc...'.format(apptype=app.SearchType))
+                lrc = app.parselrc(app.lrc)
+                return lrc
 
 
 if __name__ == '__main__':
-
     for root, dirs, files in os.walk(musicpath):
         for filepath in files:
             the_path = os.path.join(root, filepath)
-            # the_path='E:\Music_back\The Beatles\Hey Jude\The Beatles - Hey Jude.mp3'
             if (the_path.find("mp3") != -1):
                 music = eyed3.load(the_path)
                 artist = music.tag._getArtist()
                 title = music.tag._getTitle()
                 tool = MusicTools()
-                lyric = tool.GetLyric(title, artiist=artist)
+                lyric = tool.GetLyric(title, artist)
                 if lyric:
-                    print "get lyric!!!!!!!!!"
-                    lyric = lyric.decode('utf8')
-                    lyric = u''.join(lyric)
-                    lyric = unicode(lyric)
-                    # mylrcstr=mylrcstr.encode('utf-8')
-
+                    print("get lyric!!!!!!!!!")
+                    print(lyric)
                     music.tag.lyrics.set(lyric)
-                    # music.tag.lyrics[0].set(mylrcstr)
                     music.tag.save()
-                    # print lyric
-    print 'the end!'
-    """
-    print 'the end!'
-    name = "阴天快乐"
-    artiist = "陈奕迅"
-    # xiami = XiaMiLrc(name, artiist)
-    # url = xiami.Search()
-    # lyric = xiami.GetLyric(url)
-    # print lyric
-    # ttpod=TTpodLyric(name,artiist)
-    # lyric=ttpod.GetLyric()
-
-    baidu = BaiDuMusic(name, artiist)
-    url = baidu.Search()
-    lyric = baidu.GetLyric(url)
-
-
-    # parser = LyricTools(lyric=lyric)
-    # data = parser.parseLyric()
-    # print data
-    """
+    print('the end!')
