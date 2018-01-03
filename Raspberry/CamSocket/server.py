c34a199a35372c9278797dd5612e52ce24b094ea
liangliang <liangliangyy@gmail.com>
2018-01-03 22:25:13 +0800

    update server script

--- Raspberry/CamSocket/server.py
+++ Raspberry/CamSocket/server.py
@@ -13,7 +13,6 @@
 @time: 2017/5/22 下午9:25
 """
 
-import io
 import socket
 import struct
 from PIL import Image
@@ -30,37 +29,13 @@ server_socket.listen(0)
 
 connection = server_socket.accept()[0].makefile('rb')
 
-#
-# try:
-#     while True:
-#
-#         image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-#         print(image_len)
-#         if not image_len:
-#             break
-#
-#         image_stream = io.BytesIO()
-#         image_stream.write(connection.read(image_len))
-#
-#         image_stream.seek(0)
-#         image = Image.open(image_stream)
-#         cv2img = numpy.array(image, dtype=numpy.uint8)
-#         print('Image is %dx%d' % image.size)
-#         image.save('test.jpg')
-#         cv2.imshow('frame', cv2img)
-#         cv2.waitKey(10)
-# finally:
-#     connection.close()
-#     server_socket.close()
-
-
 PAGE = """\
 <html>
 <head>
-<title>picamera MJPEG streaming demo</title>
+<title>camera MJPEG streaming demo</title>
 </head>
 <body>
-<h1>PiCamera MJPEG Streaming Demo</h1>
+<h1>Camera MJPEG Streaming Demo</h1>
 <img src="stream.mjpg" width="640" height="480" />
 </body>
 </html>
@@ -103,10 +78,9 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Pragma', 'no-cache')
             self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
             self.end_headers()
-            try:
 
-                while True:
-
+            while True:
+                try:
                     image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
                     print(image_len)
                     if not image_len:
@@ -118,18 +92,19 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     image_stream.seek(0)
                     image = Image.open(image_stream)
                     cv2img = numpy.array(image, dtype=numpy.uint8)
+                    imgbuffer = image_stream.getvalue()
                     self.wfile.write(b'--FRAME\r\n')
                     self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(image_stream.getvalue()))
+                    self.send_header('Content-Length', len(imgbuffer))
                     self.end_headers()
-                    self.wfile.write(image_stream.getvalue())
+                    self.wfile.write(imgbuffer)
                     self.wfile.write(b'\r\n')
 
 
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
+                except Exception as e:
+                    logging.warning(
+                        'errror streaming client %s: %s',
+                        self.client_address, str(e))
 
         else:
             self.send_error(404)
@@ -140,13 +115,11 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-    # camera.led = False
-
 
 output = StreamingOutput()
 
 try:
-    address = ('127.0.0.1', 8000)
+    address = ('', 8000)
     server = StreamingServer(address, StreamingHandler)
     server.serve_forever()
 except Exception as e:
